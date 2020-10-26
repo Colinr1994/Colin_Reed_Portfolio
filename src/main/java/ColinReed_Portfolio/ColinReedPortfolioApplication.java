98198ab90224c4faa4c9fe4165fc4af93212d64c
Colinr1994 <colinr1994@gmail.com>
2020-10-25 17:38:45 -0700

    init

---
+++ src/main/java/ColinReed_Portfolio/ColinReedPortfolioApplication.java
@@ -0,0 +1,13 @@
+package ColinReed_Portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColinReedPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColinReedPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ColinReed_Portfolio/ColinReedPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package ColinReed_Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColinReedPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
